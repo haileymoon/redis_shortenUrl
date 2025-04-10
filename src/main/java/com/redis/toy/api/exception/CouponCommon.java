dc842018551601ef3f0e370e381d9f32d0023e41
hailey <haileymoon@netmarble.com>
2025-04-10 14:05:00 +0900

    feat: 쿠폰 에러코드 맥이기

---
+++ src/main/java/com/redis/toy/api/exception/CouponCommon.java
@@ -0,0 +1,71 @@
+package com.redis.toy.api.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum CouponCommon {
+	OK(200, "SUCCESS", 200)
+	, INVALID_PARAMETER(4000, "Invalid parameter.", HttpStatus.BAD_REQUEST.value())
+	, NOT_ALLOWED(4050, "Not allowed", HttpStatus.METHOD_NOT_ALLOWED.value())
+
+	, AUTH_NOT_LOGGED_IN(21001, "로그인이 필요합니다.", 404)
+	, AUTH_NOT_EXISTS_PID(21002, "회원번호 확인 후 다시 시도해주세요.", 400)
+	, AUTH_NOT_EXISTS_USER(21002, "회원번호 확인 후 다시 시도해주세요.", 400)
+
+	, INVALID_PARAMETER_GAME_CODE(22001, "게임코드를 입력해주세요.", 400)
+	, INVALID_PARAMETER_LANGUAGE_CD(22002, "언어코드를 입력해주세요.", 400)
+	, INVALID_PARAMETER_PID(22003, "플레이어 ID를 입력해주세요.", 400)
+	, INVALID_PARAMETER_COUPON_CODE(22004, "쿠폰코드를 입력해주세요.", 400)
+	, INVALID_PARAMETER_REWARD(22005, "아이템 선택 정보가 유효하지 않습니다.", 400)
+	, INVALID_PARAMETER_REWARD_SELECTED(22006, "아이템을 선택해주세요.", 400)
+
+	, NO_DATA_GAME_META(23001, "진행 중인 쿠폰 이벤트가 존재하지 않습니다.", 400)
+	, NO_DATA_GAME_CONTENTS(23002, "게임 컨텐츠 데이터가 존재하지 않습니다.", 400)
+	, NO_DATA_GAME_WEBPAGE_INFO(23003, "게임 웹페이지 데이터가 존재하지 않습니다.", 400)
+	, NO_DATA_REWARD_INFO(23004, "보상 정보 조회 중 오류가 발생했습니다.", 400)
+	, NO_DATA_GAME_WEBPAGE_PID(23005, "플레이어 ID 위지 안내 데이터가 존재하지 않습니다.", 400)
+
+	, COUPON_RESTRICT(24001, "쿠폰 번호 입력을 연속 10회 실패하셨습니다. 1시간 후 다시 입력 가능합니다.", 400)
+	, COUPON_WRONG(24002, "잘못된 쿠폰 번호입니다. 쿠폰을 다시 확인한 후 입력해 주세요.", 400)
+	, COUPON_PERIOD_EXPIRATION(24003, "이미 쿠폰을 사용하였거나, 유효기간이 지난 쿠폰입니다. 쿠폰을 다시 확인한 후 입력해 주세요", 400)
+	, COUPON_EXCEED_LIMIT(24004, "해당 쿠폰의 교환 횟수를 초과하였습니다.", 400)
+	, COUPON_NON_TARGET_USER(24005, "해당 쿠폰 사용 가능한 대상자가 아닙니다.", 400)
+	, COUPON_EXHAUST(24006, "준비된 쿠폰이 모두 소진되었습니다.", 400)
+
+	, COUPON_NOT_ALLOWED_WORLD(24007, "해당 월드에서 사용이 불가능한 쿠폰입니다.", 400)
+	, COUPON_NOT_ALLOWED_REGION(24008, "해당 리전에서 사용이 불가능한 쿠폰입니다.", 400)
+	, COUPON_NOT_ALLOWED_COUNTRY(24009, "해당 국가에서 사용이 불가능한 쿠폰입니다.", 400)
+	, COUPON_NOT_ALLOWED_MARKET(24010, "해당 마켓에서 사용이 불가능한 쿠폰입니다.", 400)
+	, COUPON_NOT_ALLOWED_BUILD(24011, "해당 빌드에서 사용이 불가능한 쿠폰입니다.", 400)
+
+	, COUPON_PROVIDE_FAIL(24012, "아이템 지급 중 오류가 발생했습니다.", 500)
+	, COUPON_PROVIDE_SUCCESS(200, "아이템이 지급되었습니다.", 200)
+
+	, JSON_PARSE_ERROR(99993, "데이터 변환 중 오류가 발생했습니다.", 400)
+	, NO_DATA(99994, "데이터를 찾을 수 없습니다.", 500)
+	, INVALID_ACCESS(99995, "잘못된 접근입니다.", 500)
+	, ERROR_DB_INSERT(99996, "데이터 저장 중 오류가 발생했습니다.", 500)
+	, UNKNOWN_ERROR(99999, "예기치 못한 오류가 발생했습니다. 잠시 후에 다시 시도해주세요.", 500);
+
+	private final int errorCode;
+	private final String errorMessage;
+	private final int httpStatus;
+
+	CouponCommon(final int code, final String msg, final int status) {
+		this.errorCode = code;
+		this.errorMessage = msg;
+		this.httpStatus = status;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public int getHttpStatus() {
+		return httpStatus;
+	}
+}
+
